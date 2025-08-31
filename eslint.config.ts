b4acf24c41e42d9000565b66a8f9b6bc1b41e9cb
goooooonight <fyf200436@gmail.com>
2025-08-31 22:47:51 +0800

    chore:修改了eslint组件命名规范，允许单个单词命名

--- eslint.config.ts
+++ eslint.config.ts
@@ -47,12 +47,7 @@ export default defineConfigWithVueTs(
       'prettier/prettier': 'error',
 
       // 黑马文档部分额外规则
-      'vue/multi-word-component-names': [
-        'warn',
-        {
-          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
-        }
-      ],
+      'vue/multi-word-component-names': 'off', // 关闭vue组件名称多单词组成的检查
       'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
       // 💡 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
       'no-undef': 'error'
