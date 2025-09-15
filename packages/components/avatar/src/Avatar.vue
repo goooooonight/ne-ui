0bbed9c344d7d4c0d19ece4d8fb4f866b3a9a61c
goooooonight <fyf200436@gmail.com>
2025-09-15 19:35:32 +0800

    feat(avatar)：新增了avatar组件的shape形状和size尺寸属性

--- packages/components/avatar/src/Avatar.vue
+++ packages/components/avatar/src/Avatar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { createNameSpace } from '@ne-ui/utils'
 import { computed } from 'vue'
-// import { avatarProps } from './avatar'
+import { avatarProps } from './avatar'
 
 // 组件命名
 defineOptions({ name: 'ne-avatar' })
@@ -9,14 +9,28 @@ defineOptions({ name: 'ne-avatar' })
 // 生成命名空间
 const ns = createNameSpace('avatar')
 
+// 获取 props
+const props = defineProps(avatarProps)
+
 // 生成自定义类名
 const classCustom = computed(() => {
-  return [ns.b()]
+  const { shape, size } = props
+  return [ns.b(), ns.m(shape), typeof size === 'string' && ns.m(size)]
+})
+
+// 生成内联样式
+const style = computed(() => {
+  // 当 size 为 number 类型时，自定义宽高
+  return typeof props.size === 'number'
+    ? { width: `${props.size}px`, height: `${props.size}px` }
+    : undefined
 })
 </script>
 
 <template>
-  <span :class="classCustom"></span>
+  <span :class="classCustom" :style="style">
+    <slot></slot>
+  </span>
 </template>
 
 <style scoped></style>
---
+++ packages/components/avatar/src/type.d.ts
@@ -0,0 +1,3 @@
+export type Shape = 'circle' | 'square'
+
+export type Size = 'small' | 'default' | 'large'
